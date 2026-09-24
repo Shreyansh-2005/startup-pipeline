e70eefdc1363b4024939d4a90729eaebd4e4ad8f
Shreyansh-2005 <shreyanshdixithdi@gmail.com>
2026-09-25 00:05:35 +0530

    fix

--- pipeline.cjs
+++ pipeline.cjs
@@ -1,9 +1,11 @@
 const fs = require('fs');
-const envFile = fs.readFileSync('.env', 'utf8');
-envFile.split('\n').forEach(line => {
-  const [key, value] = line.split('=');
-  if (key && value) process.env[key.trim()] = value.trim();
-});
+if (fs.existsSync('.env')) {
+  const envFile = fs.readFileSync('.env', 'utf8');
+  envFile.split('\n').forEach(line => {
+    const [key, value] = line.split('=');
+    if (key && value) process.env[key.trim()] = value.trim();
+  });
+}
 
 const axios = require('axios');
 const xml2js = require('xml2js');
